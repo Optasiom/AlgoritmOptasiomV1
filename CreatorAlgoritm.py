27cb8eb3d58dae678f44a2dd962e1797143a86e3
Optasiom <136899039+Optasiom@users.noreply.github.com>
2023-10-25 08:28:14 -0700

    algoritm

---
+++ CreatorAlgoritm.py
@@ -0,0 +1,246 @@
+from os import system
+from time import sleep
+from random import choice, randint
+try:
+    import pyfiglet
+except:
+    system("pip install pyfiglet")
+try:
+    import rainbowtext
+except:
+    system("pip install rainbowtext")
+# - colors
+green = "\033[32m"
+red = "\033[31m"
+blue = "\033[36m"
+pink = "\033[35m"
+yellow = "\033[93m"
+darkblue = "\033[34m"
+white = "\033[00m"
+mark = '\x1b[38;5;4m'
+mark1 = '\x1b[48;5;15m'
+mark2 = '\x1b[0m'
+# tancks!
+def Aolgo():
+    num1 : str = (randint(0,9))
+    num2 : str = (randint(0,9))
+    num3 : str = (randint(0,9))
+    num4 : str = (randint(0,9))
+    num5 : str = (randint(0,9))
+    num6 : str = (randint(0,9))
+    num7 : str = (randint(0,9))
+    num8 : str = (randint(0,9))
+    num9 : str = (randint(0,9))
+    num10 : str = (randint(0,9))
+    num11 : str = (randint(0,9))
+    num12 : str = (randint(0,9))
+    num13 : str = (randint(0,9))
+    num14 : str = (randint(0,9))
+    num15 : str = (randint(0,9))
+    num16 : str = (randint(0,9))
+    num17 : str = (randint(0,9))
+    num18 : str = (randint(0,9))
+    num19 : str = (randint(0,9))
+    num20 : str = (randint(0,9))
+    num21 : str = (randint(0,9))
+    num22 : str = (randint(0,9))
+    ipn1 : str = (randint(0,9))
+    ipn2 : str = (randint(0,9))
+    ipn3 : str = (randint(0,9))
+    ipn4 : str = (randint(0,9))
+    ipn5 : str = (randint(0,9))
+    ipn6 : str = (randint(0,9))
+    ipn7 : str = (randint(0,9))
+    ipn8 : str = (randint(0,9))
+    ipn9 : str = (randint(0,9))
+    ip = f"5.{ipn1}{ipn2}{ipn3}.{ipn4}{ipn5}{ipn6}.{ipn7}{ipn8}{ipn9}"
+    server1 : list = ['.Porn(/yft.fata-Tehran.CyberPolice.Bov.ir)*Xnxx.com(rubika-ip.1.1.1.1)','*(\Porn.Server-Font:Tahoma.Porn.Google.sexy:posted/-!).','.Sex-WWW.xVideo.com/-Filtering.video.porns(/NetReport.af.of.af.of.af.0/)','(#Porn%VideoPorn%pornhub%porn/Yftt18-ship.roll:porn)/LoLiKidPorn.DarkWeb.Onion(Tor.af.af.of.af.af.af.af)','.tel:999-Sex/Suppoer.fake(org.android.sex)/Porn.Videos','.hacked-Phones(/account.hacked.and.fake.account/-!.Porn/)Porn:PostRubino.PornHub.com(:3)']
+    bug1 : list = ['.org','.revers.Yftt15k.in','.porno','.us','.kporn','.Terror','.VPN','.onion']
+    hasas1 : list = ['/porn.ly/','/onion.porn/','/dark.web/','/persian.sex/','/client.hacking/','/camera.hack.links/','/Sigma.C.S.C/pornhub/','/zed.dildo.shops/','/porno.rubino/','/rubika.hacked/','/videos.not.wanted/','/have.fucking.girls/','/onion.porn.kids/','/110.fake-support(/porno/)/','/posted:fuck.leader.islamic.ae/','/Hello.porn.r/','/beta.version.3.5.7/apk.pornhub.com/','/page.rubino.porn.video/','/porn.ir.rubino/','/porn.us/r.rubika.ir/','/sexy.post-rubino/']
+    server : str = choice(server1)
+    bug : str = choice(bug1)
+    hasas : str = choice(hasas1)
+    try:
+        system('clear')
+    except:
+        system('cls')
+    banner_ol=f"{num1}.{num2}.{num3}.{num4}{server}{num5}.{num6}.{num7}.{num8}.{num9}.{num10}.{num11}.{num12}.{num13}.{num14}.{num15}.{num16}{bug}/{num17}.{num18}.{num19}.{num20}{hasas}{num21}.{num22}"
+    pr_ol=f"""{blue}
+    This Algoritm (Creator Algoritm.Red>Attcker : OPTASIOM) Rubika -> @the_optasiom {white}
+    
+    {red}Algoritm.Red>Attacker : {green}'{darkblue}{banner_ol}.{ip}{green}'{white}
+    \n\n {pink}
+    Good Bye{white}
+    """
+    for i in pr_ol:
+        print(i, flush=True, end='')
+        sleep(0.002)
+#-----------------
+def Tor():
+    import datetime
+    now = datetime.datetime.now()
+    time = now.strftime("%H.%M/%S:10.000")
+    try:
+        system('clear')
+    except:
+        system('cls')
+    pr_tor=f""""{blue}
+    This Code Tor Hasas (Creator Tor.Black>Jp : OPTASIOM) {green}   start {white}
+    
+    {darkblue}
+    Tor Hasas : {green}'{red}tor.Jp(1.52.1.0).Tor.af.af.of.of.of.af.jp.jp({time})jump.Vpn/loading:(-pornhub.com-)(6.21.4.1){green}'
+{white}
+
+    \n\n\n\n{pink}
+    Good Bye!{white}
+    """
+    for i in pr_tor:
+        print(i, flush=True, end='')
+        sleep(0.002)
+def bugs():
+    try:
+        system('clear')
+    except:
+        system('cls')
+    pr_bg=f"""{darkblue}
+    This Gods Bugs! ( Creator All Bugs(Alfa>Attacker) : OPTASIOM){white}
+    
+    {red}
+    
+    ------------------
+    O
+    p
+    \\porn
+    #Rubino
+    T*
+    #P
+    ------------------
+    G
+    T
+    -Y
+    #P.O.R.N
+    -Re*v*V*P
+    ------------------
+    P
+    -O
+    #R/S-E-X
+    S3
+    ------------------
+    -M#
+    #I-
+    6-US
+    #Rubino><Porno
+    ------------------
+    -C*
+    *S-
+    -C*
+    #Dark.onion
+    ------------------
+    \G:
+    :T\\
+    \F:
+    #UFT.Tw-porn
+    ------------------
+    -C*
+    *A-
+    =M*
+    ./#P
+    ------------------
+    -[S]
+    *E*
+    *S*
+    _-/I
+    ------------------
+    1214(4241.4141.5150-PP[1[2[3[4[5[0]-5]-4]-3]-2]-1])
+    _S-*
+    _I^
+    #P!999
+    ------------------
+    #R-
+    -t#
+    #S-
+    #onion_TOR
+    ------------------
+    @99999 _ @231784 _ @14147
+    :3
+    0:
+    #Porno
+    ------------------
+    A-
+    -F
+    O-
+    #SEX_XXX_RUBINO
+    ------------------
+    -y   #
+    t#
+    #s
+    #00000
+    ------------------
+    #F
+    U
+    C
+    K
+    #P
+    o
+    r
+    n
+    ------------------{pink}
+    Good Bye!{white}
+    """
+    for i in pr_bg:
+        print(i, flush=True, end='')
+        sleep(0.002)
+class startingDifis:
+    def er(self,sendNum):
+        
+        sleep(0.5)
+        print("\r Started...                                         ",end="",flush=False)
+        print ('\n\n')
+        print(f"\r {green}Loading",end="",flush=False)
+        sleep(1)
+        print(f"\r {red}Loading.",end="",flush=False)
+        sleep(1)
+        print(f"\r {blue}Loading..",end="",flush=False)
+        sleep(1)
+        print(f"\r {yellow}Loading...",end="",flush=False)
+        sleep(1)
+        print(f"\r {darkblue}Load!",end="",flush=False)
+        sleep(0.5)
+        if sendNum==1:
+            Aolgo()
+        elif sendNum==2:
+            Tor()
+        elif sendNum==3:
+            bugs()
+start=startingDifis()
+text=pyfiglet.figlet_format(text="optasiom")
+text=rainbowtext.text(text)
+print(text)
+while 1:
+    print("""
+    1.Creater Algoritm
+    2.Creater Tor hasas
+    3.Bugs!
+    0.Exit
+    """)
+    try:
+        s=int(input(f"{blue}select - >{red}"))
+        {white}
+    except:
+        print(f"{red}Erorr select! ")
+        {white}
+        break
+    {white}
+    if s==0:
+        print("ByeBye")
+        break
+    elif s==1:
+        start.er(1)
+    elif s==2:
+        start.er(2)
+    elif s==3:
+        start.er(3)
+    else:
+        print(f"{red}Error select...")
+        {white}
+        break
